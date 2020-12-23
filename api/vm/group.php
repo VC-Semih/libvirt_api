95d16ccf814604ea7309d5b1eb110afc2e6e95af
VC-Semih <semihciftci.pro@gmail.com>
2020-12-23 21:12:51 +0100

    group.php bug fix

--- api/vm/group.php
+++ api/vm/group.php
@@ -105,17 +105,19 @@ function deleteGroup($group_name){
 function createGroupFolder(){
     global $group_dir;
     if (!file_exists($group_dir)) {
-        mkdir($group_dir, 0777, true);
+        if (!mkdir($group_dir, 0777, true)) {
+            verbose(0, "Failed to create $group_dir");
+        }
     }
 }
 
 
 if (basename($_SERVER['PHP_SELF']) === basename(__FILE__)) { // Check if the link corresponds to the file (and not another api that made require() )
     $request_method = $_SERVER['REQUEST_METHOD'];
-    createGroupFolder();
     switch ($request_method) {
         case 'GET':
             // Retrive VM state
+            createGroupFolder();
             getGroups();
             break;
         case 'PUT':
